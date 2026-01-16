d9755846f3e8396a53ea14888fcc5ce3f3b4b4f3
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-01-16 15:19:18 +0100

    feat: implemento show

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,6 +14,27 @@ function index(req, res) {
     });
   });
 }
+/* SHOW */
+function show(req, res) {
+  const id = req.params.id;
+
+  const sql = "SELECT * FROM posts WHERE id = ?";
+  connection.query(sql, [id], (error, resalts) => {
+    if (error) {
+      return res.status(500).json({
+        message: "server error 500",
+      });
+    }
+    if (resalts.length === 0) {
+      return res.status(404).json({
+        message: "Risorsa non trovata",
+      });
+    }
+    res.json({
+      resalt: resalts[0],
+    });
+  });
+}
 
 /* DESTROY */
 function destroy(req, res) {
@@ -33,6 +54,7 @@ function destroy(req, res) {
 const controller = {
   index,
   destroy,
+  show,
 };
 
 export default controller;
--- routers/posts.js
+++ routers/posts.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* INDEX */
 router.get("/", postController.index);
 /* SHOW */
-
+router.get("/:id", postController.show);
 /* DESTROY */
 router.delete("/:id", postController.destroy);
 
