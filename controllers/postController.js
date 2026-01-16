bdb662fd553f897e1cea9127880dc081d28e070c
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-01-16 16:44:11 +0100

    feat: bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,7 +14,7 @@ function index(req, res) {
     });
   });
 }
-/* SHOW */
+/* show */
 function show(req, res) {
   const id = req.params.id;
 
@@ -29,10 +29,23 @@ function show(req, res) {
       return res.status(404).json({
         message: "Risorsa non trovata",
       });
+    } else {
+      const sqlTags =
+        "SELECT tags.label, tags.id FROM  tags INNER JOIN post_tag ON tags.id = post_tag.tag_id where post_tag.post_id = ?";
+      connection.query(sqlTags, [id], (error, resaltTags) => {
+        if (error) {
+          return res.status(500).json({
+            message: "server error 500",
+          });
+        }
+        const post = {
+          ...resalts[0],
+          tags: resaltTags,
+        };
+
+        res.json(post);
+      });
     }
-    res.json({
-      resalt: resalts[0],
-    });
   });
 }
 
